61f82a636e6437b10bd77c2cdc0278c7a1700d4a
Jinesh K <jineshjain6907@gmail.com>
2026-04-29 10:43:00 +0530

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to the world of Python.")
+
+age = input("How old are you? ")
+print(f"Wow, {age} years old! That's great, {name}.")
+
+print("Goodbye!")
